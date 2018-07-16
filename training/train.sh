bc4d43d7a1a7edee796a2600320fecc24f5d8f7d
SanjunLiu <sanjun_liu@sina.com>
2018-07-16 20:44:34 +0800

    add train shell

---
+++ training/train.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -e
+
+CAFFE=./caffe
+SOLVER=./training/net2/vanilla_adam_solver.prototxt
+WEIGHTS=./model/net2/_iter_150000.caffemodel
+#SNAPSHOT=_iter_110000.solverstate
+
+#$CAFFE train  --solver=$SOLVER --snapshot=$SNAPSHOT --gpu 1,2 \
+#$CAFFE train --solver=$SOLVER --gpu 1,2 \
+$CAFFE train  --solver=$SOLVER --weights=$WEIGHTS --gpu 1 \
+    2>&1 | tee output_tmp/train_5p_log.log
